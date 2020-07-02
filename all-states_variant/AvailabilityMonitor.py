a36138d165e5ad58ec1ca32f5abe622b21f2f713
Radu <radu.stefan@dynatrace.com>
2020-07-02 13:50:18 +0100

    new variant; fixed malformed zip;

---
+++ all-states_variant/AvailabilityMonitor.py
@@ -0,0 +1,54 @@
+import requests
+from ruxit.api.base_plugin import RemoteBasePlugin
+from time import time
+import logging
+
+logger = logging.getLogger(__name__)
+
+
+class PluginMain(RemoteBasePlugin):
+    def initialize(self, **kwargs):
+        logger.info(f"Using config: {self.config}")
+        self.token = self.config.get('api_token')
+        self.tenant = self.config.get('tenant_id')
+        self.timeframe = self.config.get('timeframe')
+
+    def query(self, **kwargs):
+        env_api = f"https://{self.tenant}.live.dynatrace.com/api/v1"
+        auth = dict(Authorization=f"Api-Token {self.token}")
+
+        # Make API call, retrieve unmonitored hosts.
+        nextPageKey = 1
+        hosts = []
+
+        logger.info("Parsing tenant")
+        # Loop until whole tenant was parsed in case there's more than 1 page of results
+        while nextPageKey:
+            query = f"?relativeTime={self.timeframe}&availabilityState=UNMONITORED"
+            if nextPageKey != 1:
+                query += f"?{nextPageKey}"
+
+            response = requests.get(
+                url=env_api+"/oneagents"+query, headers=auth).json()
+
+            if len(response.get('hosts')) > 0:
+                for host in response.get('hosts'):
+                    hosts.append(host.get('hostInfo').get('entityId'))
+
+            nextPageKey = response.get('nextPageKey')
+
+        # Push events to the offending hosts (if any)
+        if len(hosts) > 0:
+            logger.info("Sending an event.")
+            for host in hosts:
+                event = dict(eventType="AVAILABILITY_EVENT",
+                             start=round(time())/1000,
+                             timeoutMinutes=5,
+                             attachRules=dict(entityIds=[host]),
+                             description="Host detected as Unmonitored",
+                             title="Host state changed to Unmonitored",
+                             source="OneAgent Availability Monitor"
+                             )
+                requests.post(url=env_api+"/events", json=event, headers=auth)
+        else:
+            logger.info("No events to push.")
---
+++ all-states_variant/README.md
@@ -0,0 +1,4 @@
+# OneAgent Availability Monitoring Extension - All States Variant
+This is a variant of the main plugin which alerts on any state changes.
+
+Documentation to follow.
